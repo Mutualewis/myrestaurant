fa9cfa90e5018df179dca9f47fc4c092277e1467
mutualewis <mutualewis@gmail,com>
2019-08-28 23:12:51 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -2,104 +2,113 @@ import React from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 
-class App extends React.Component {
-     constructor() {
-      super();
-      this.state = {
-         data: 
-         [
-            {
-               "name":'Salad',
-               "choices": [
-                  {"name": 'Santa Fe'},
-                  {"name": 'Greek'},
-                  {"name": 'Asian'},
-               ],
-               "related": [
-                 {
-                    "name":'Dressing',
-                    "choices": [
-                      {"name": 'Italian'},
-                      {"name": 'Blue Cheese'},
-                      {"name": 'Ranch'},
-                    ]
-                 },
-                 {
-                    "name":'Bread',
-                    "choices": [
-                      {"name": 'Italian'},
-                      {"name": 'Flat'},
-                      {"name": 'Sourdough'},
-                    ]                  
-                 }
+const items = [
+{
 
-               ]
-            },
-            {
-               "name":'Entree',
-               "choices": [
-                  {"name": 'Steak'},
-                  {"name": 'Salmon'},
-                  {"name": 'Rice'},
-               ],
-               "related": [
-               ]
-            },
-            {
-               "name":'Soup',
-               "choices": [
-                  {"name": 'Minestrone'},
-                  {"name": 'Hot and sour'},
-                  {"name": 'Miso'},
-               ],
-               "related": [
-                 {
-                    "name":'Bread',
-                    "choices": [
-                      {"name": 'Breadsticks'}
-                    ]
-                 }
-               ]
-            },
-            
-         ]
-      }   
-   };
-   updateState(e) {
-      this.setState({data: e.target.value});
-   }  
-   render() {
-      let elements = this.state.data.map((element) => {
-          return (<li key={element.name}><Content 
-                  key = {element.name} componentData = {element}/></li>)
-      })
-      let choices = this.state.data.map((element) => {
-          return (<li key={element.name}><Content 
-                  key = {element.name} componentData = {element} componentState = {this.updateState} /></li>)
-      })
+     "name":'Salad',
+     "choices": [
+        {"name": 'Santa Fe'},
+        {"name": 'Greek'},
+        {"name": 'Asian'},
+     ],
+     "related": [
+       {
+          "name":'Dressing',
+          "choices": [
+            {"name": 'Italian'},
+            {"name": 'Blue Cheese'},
+            {"name": 'Ranch'},
+          ]
+       },
+       {
+          "name":'Bread',
+          "choices": [
+            {"name": 'Italian'},
+            {"name": 'Flat'},
+            {"name": 'Sourdough'},
+          ]                  
+       }
 
-      return (
-         <div>
-            <Header/>
-            <ul>{elements}</ul>
-         </div>
-      );
-   }
+     ]
+  },
+  {
+     "name":'Entree',
+     "choices": [
+        {"name": 'Steak'},
+        {"name": 'Salmon'},
+        {"name": 'Rice'},
+     ],
+     "related": [
+     ]
+  },
+  {
+     "name":'Soup',
+     "choices": [
+        {"name": 'Minestrone'},
+        {"name": 'Hot and sour'},
+        {"name": 'Miso'},
+     ],
+     "related": [
+       {
+          "name":'Bread',
+          "choices": [
+            {"name": 'Breadsticks'}
+          ]
+       }
+     ]
+  },
+
+];
+
+function ListItem({ item }) {
+  let choices = null;
+  let related = null;
+  if (item.choices && item.choices.length) {
+    choices = (
+      <ul>
+        {item.choices.map(i => (
+          <ListItem item={i} key={i.id} />
+        ))}
+      </ul>
+    );
+  }
+  if (item.related && item.related.length) {
+    related = (
+      <ul>
+        {item.related.map(i => (
+          <ListItem item={i} key={i.id} />
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <li>
+      <input type = "checkbox" value = {item.name} /> {item.name}
+      {choices}
+      {related}
+    </li>
+  );
 }
+
+function App() {
+  return (
+   <div>
+      <Header/>
+      <ul>
+        {items.map(i => (
+          <ListItem item={i} key={i.id} />
+        ))}
+      </ul>
+   </div>
+  );
+}
+
 class Header extends React.Component {
    render() {
       return (
          <div>
             <h1>Restaurant Menu</h1>
-         </div>
-      );
-   }
-}
-class Content extends React.Component {
-   render() {
-      return (
-         <div>
-            <input type = "checkbox" value = {this.props.componentData.name} onChange = {this.componentState} /> {this.props.componentData.name}
          </div>
       );
    }
